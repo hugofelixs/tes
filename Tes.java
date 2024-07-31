4a09a64a3154e3f10cbcc4cc77adb74da29027cd
Hugo Felix Satriyawan <hugo_satriyawan_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:12:50 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "Felix";
+        String kelas = "X RPL 5";
+        System.out.print("halo, nama saya " + nama + " dari kelas " +kelas);
+    }
+}
